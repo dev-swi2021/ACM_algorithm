708629b72531c5e3a1c6965c71546c66668de88b
dev-swi2021 <77826604+dev-swi2021@users.noreply.github.com>
2021-02-21 19:09:06 +0900

    Update programmers_0220_1.py

--- programmers_0220_1.py
+++ programmers_0220_1.py
@@ -1,3 +1,4 @@
+# sol1
 def solution(n, arr1, arr2):
     answer = []
     
@@ -14,9 +15,22 @@ def solution(n, arr1, arr2):
                 tmp +='#'
             else:
                 tmp += ' '
-        answer.append(tmp)
-    
+        answer.append(tmp)    
     return answer
+
+
+# sol2
+def solution(n, arr1, arr2):
+    answer = []
+    
+    for a1, a2 in zip(arr1, arr2):
+        a = bin(a1|a2)[2:]
+        a = a.rjust(n, '0')
+        a = a.replace('1', '#')
+        a = a.replace('0', ' ')
+    answer.append(a)
+return answer
+
 
 # Test
 n = [5, 6]
