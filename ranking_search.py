cf59efeeb1c5e629280debb9ccdaea8cf0a8ef54
dev-swi2021 <77826604+dev-swi2021@users.noreply.github.com>
2021-03-03 18:40:12 +0900

    Update ranking_search.py

--- ranking_search.py
+++ ranking_search.py
@@ -45,3 +45,12 @@ def solution(info, query):
         else:
             answer.append(0)
     return answer
+
+# Test
+info=[["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"]]
+query = [["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]]
+result = [[1, 1, 1, 1, 2, 4]]
+
+for i, q, r in zip(info, query, result):
+    res = solution(i,q)
+    print("{} {} ==> {}".format(r, res, res == r))
